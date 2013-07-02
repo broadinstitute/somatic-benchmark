5afd6e067abf483fe6a4a4cd472c65f4a9af9a43
Louis Bergelson <louisb@broadinstitute.org>
2013-07-02 14:44:36 -0400

    Putting the right create_1g_sim_data.pl in.  Adding a bam_info file as an example.

---
+++ create_1g_sim_data.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+use strict;
+use IO::File;
+
+my $bam = "/humgen/gsa-hpprojects/NA12878Collection/bams/CEUTrio.HiSeq.WGS.jaffe.b37_decoy.NA12878.bam";
+my $ref = "/humgen/1kg/reference/human_g1k_v37_decoy.fasta";
+my @libraries = ("Solexa-18483","Solexa-18484","Solexa-23661");
+my $interval = "chr20.interval_list";
+my $pieces = 6;
+my $outdir = "data_1g_wgs";
+
+my $QUEUE = "week";
+foreach my $library (@libraries) {
+    my $cmd = "./fracture_bam.pl $bam $ref $library $interval $pieces $outdir"; 
+    print $cmd . "\n";
+    system($cmd) == 0 or die();
+}

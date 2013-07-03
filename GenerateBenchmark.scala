ba26315927f11776f2c4cdb23847e3a25ef5544b
Louis Bergelson <louisb@broadinstitute.org>
2013-07-03 17:21:49 -0400

    Working on generate benchmark.  It is currently in a totally broken transition state.

--- GenerateBenchmark.scala
+++ GenerateBenchmark.scala
@@ -1,8 +1,6 @@
 package org.broadinstitute.sting.gatk.queue.qscripts.examples
 
-import org.broadinstitute.sting.gatk.CommandLineGATK
-import org.broadinstitute.sting.gatk.walkers.genotyper.UnifiedGenotyper
-import org.broadinstitute.sting.gatk.walkers.variantutils.SelectVariants
+
 import org.broadinstitute.sting.queue.QScript
 import org.broadinstitute.sting.queue.extensions.gatk._
 
@@ -22,19 +20,54 @@ class GenerateBenchmark extends QScript {
   def script() {
 
     val mv = new make_vcfs 
-    mv.indelFile = indelFile    
+    mv.indelFile = indelFile
+    val mvOut = new File("dummyFile")
+    mv.vcfOutFile = mvOut
+    
+    val sim = new make_fn_sim_dataset
+    sim.inputFile = mvOut 
+
+    val ss = new SomaticSpike
+   
     add(mv)
     add( new create_1g_data)
-  }
+    }
 
   class make_vcfs extends CommandLineFunction{
     @Input(doc="vcf file containing indels to use as true indel sites") var indelFile : File = _
+    @Output(doc="dummy output for queue ordering") var vcfOutFile : File = _ 
     def commandLine = "%s/make_vcfs.pl %s".format(libDir, indelFile)
   }
   
-  class create_1g_data extends CommandLineFunction{
-    def commandLine = "%s/create_1g_data.pl".format(libDir)
+  class make_fn_sim_dataset extends CommandLineFunction{
+    @Input(doc="dummy input for queue ordering") var inputFile : File = _
+    def commandLine = "%s/make_fn_sim_dataset.pl".format(libDir)
   }
 
+  class create_1g_data extends CommandLineFunction{
+    def commandLine = "%s/create_1g_sim_data.pl".format(libDir)
+  }
+
+  class SomaticSpike extends CommandLineGATK {
+    this.analysis_type = "SomaticSpike"
+  }
+
+  class makeFalseNegatives(spikeSitesVCF : File, spikeInBam : File ,alleleFractions: Set[Double], depths : Set[String])  {
+    val outputDir = new File("fn_data" )
+     
+ 	def makeFalseNegativeDataSet(alleleFractions : Set[Double], depths:Set[String]):List[SomaticSpike]{
+ 	  for {
+ 	      fraction <- alleleFractions 
+ 	      depth <- depths
+ 	  } yield (makeMixedBam(fraction, bamMap(depth)))
+
+ 	}
+ 	
+ 	def makeMixedBam(fraction: Double, tumorBams: List[File]): SomaticSpike{
+        spike = new SomaticSpike
+        spike.reference = referenceFile
+    
+    }
+  } 
 }
         

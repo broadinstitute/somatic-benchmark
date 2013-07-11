3bf13ec9b910867a9cfc5496dbc75e2ec750b22d
Louis Bergelson <louisb@broadinstitute.org>
2013-07-11 15:42:13 -0400

    Moved the tool running CommandLineFunction into it's own class with a companion.

--- RunBenchmark.scala
+++ RunBenchmark.scala
@@ -8,22 +8,51 @@ class RunBenchmark extends QScript {
       val referenceFile : File = new File("/humgen/1kg/reference/human_g1k_v37_decoy.fasta")
 
       def script() {
+        
       }
-      
 
 
-      def generateFalseNegativeCmds(toolsToTest: List[File], normal: File, tumors:List[File]):List[CommandLineFunction] = {
-        def generateFalseNegativeCmd(tool :File, normal:File, tumor:File):CommandLineFunction ={
-            val outputDir = tool.getName+"-N"+normal.getName+"-T"+tumor.getName 
-                        
-            new CommandLineFunction{
+
+             //invokes <tool> with parameters <libDir><normal><tumor><reference><outputDir>
+      class ToolInvocation extends  CommandLineFunction{
+                @Input(doc="The tool to run")
+                var tool: File = _
+
+                @Input(doc="normal sample bam")
+                var normal: File = _
+
+                @Input(doc="tumor sample bam")
+                var tumor: File = _
+
+                @Input(doc="reference fasta")
+                var reference: File = _
+
+                @Output(doc="output directory")
+                var outputDir: File =_
+                
                 def commandLine = required(tool)+
                                   required(tool.getParent)+
                                   required(normal)+
                                   required(tumor)+
                                   required(qscript.referenceFile)+
                                   required(outputDir)
-            }
+      }     
+      object ToolInvocation {
+        def apply(tool:File, normal:File, tumor:File, reference:File, outputDir:File) = {
+            val ti = new ToolInvocation
+            ti.tool = tool
+            ti.normal = normal
+            ti.tumor = tumor
+            ti.reference = reference
+            ti.outputDir = outputDir
+            ti
+        }
+      } 
+
+      def generateFalseNegativeCmds(toolsToTest: List[File], normal: File, tumors:List[File]):List[CommandLineFunction] = {
+        def generateFalseNegativeCmd(tool :File, normal:File, tumor:File):CommandLineFunction ={
+            val outputDir = new File("fn/"+tool.getName+"-N"+normal.getName+"-T"+tumor.getName)
+            ToolInvocation(tool=tool, normal=normal, tumor=tumor, reference=referenceFile, outputDir=outputDir) 
         }
         
         for{
@@ -31,5 +60,6 @@ class RunBenchmark extends QScript {
          tumor <- tumors   
         } yield generateFalseNegativeCmd(tool, normal, tumor)
       }
+
 }
 

79ee142c07f50467930692a34579945107b7a0bf
Louis Bergelson <louisb@broadinstitute.org>
2013-07-30 16:44:46 -0400

    Removing extractIndels.sh, it is is redundant with SelectVariants

--- extractIndels.sh
+++
@@ -1,9 +0,0 @@
-GATK=~/xchip/gatk-protected/dist/GenomeAnalysisTK.jar
-
-java -Xmx2g -jar $GATK \
- -R "/humgen/1kg/reference/human_g1k_v37.fasta" \
- -T SelectVariants \
- --variant $1 \
- -o "indels.vcf" \
- -L "chr20.interval_list" \
- -selectType INDEL

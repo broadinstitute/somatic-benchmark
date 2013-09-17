f1a3cfb15dcf30b8b58dc78aa4ee2742b536874e
Louis Bergelson <louisb@broadinstitute.org>
2013-09-17 14:23:17 -0400

    Adding average lines from the plot of AltReadFraction.R

--- plotAltReadFraction.R
+++ plotAltReadFraction.R
@@ -4,5 +4,11 @@ library(ggplot2)
 
 d = gsa.read.gatkreport("combo.gsareport")
 countVar <- d$CountVariants
-qplot(AltReadFraction,nVariantLoci,data=countVar, geom="line", facets=~EvalRod)
-ggsave(file="graphs/readFractionDistribution.png", height=5, width = 10)
+countVar$EvalRod <- countVar$EvalRod/2
+
+vline.data <- data.frame(z = unique(countVar$EvalRod), EvalRod=unique(countVar$EvalRod))
+
+
+p <- qplot(AltReadFraction,nVariantLoci,data=countVar, geom="point", facets=~EvalRod) + theme_bw()
+p + geom_vline(aes(xintercept = z), vline.data)
+ggsave(file="graphs/altAlleleFraction.png", height=5, width = 10)
